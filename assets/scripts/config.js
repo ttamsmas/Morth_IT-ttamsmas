b5b9ea797a0be6d7d8c0d34dcae68709e37a2855
ttamsmas <ttamsmas@gmail.com>
2020-11-06 16:04:37 -0800

    Adding / to production

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://morthinventorytracking.herokuapp.com',
+  production: 'https://morthinventorytracking.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
