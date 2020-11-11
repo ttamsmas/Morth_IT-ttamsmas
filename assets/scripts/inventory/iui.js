7bbda823a2ffdbe8792dd043a7adbedeb9495f0d
ttamsmas <ttamsmas@gmail.com>
2020-11-11 13:05:29 -0800

    Splitting GET Response Item Array into Parts to Fill Cloned Div

--- assets/scripts/inventory/iui.js
+++ assets/scripts/inventory/iui.js
@@ -26,28 +26,51 @@ const loadInventorySuccess = function (response) {
 // goal is to visualize the array as objects that can be manipulated by class
 // I think I can create a loop that adds divs within the container for each item in the array
 // How to include update and delete TBD
-console.log(response)
-
+  // const tempItem = function (response) {
+  //   for (let i = 0; i < response.length)
+  // }
+  // console.log(response.items)
+  // console.log(response.items.length)
+  const responseArray = response.items
+  console.log(responseArray[0].name)
+  const responseArrayLength = response.items.length
 // create objects for each item in the response array
-  response => {
-    for (let i = 0; i < response.length; i++) {
-      const singleObject = response[i]
-      console.log(singleObject)
+  const createNewItems = responseArray => {
+    for (let i = 0; i < responseArrayLength; i++) {
       // define the clone unique id
       const currentClone = 'clone' + i
       console.log(currentClone)
       // clone the single item div so the formatting is consistand and update/delete buttons are already there
       const mainContainer = $('.container')
+      console.log(mainContainer)
+      // clone the example div and append it to the end of the container
+      // the example div is given a unique id so it's values can be replaced
+      // this method clones the update and delete buttons so each item can have their own
       $('.singleItem').clone().prop('id', currentClone).appendTo(mainContainer)
-      $('p:nth-last-child(1)').html(singleObject[0])
-      $('p:nth-last-child(2)').html(singleObject[1])
-      $('p:nth-last-child(3)').html(singleObject[2])
-      $('p:nth-last-child(4)').html(singleObject[3])
-      $('p:nth-last-child(5)').html(singleObject[4])
-      $('p:nth-last-child(6)').html(singleObject[5])
-      $('.singleItem:nth-last-child(1)').show()
+      // select the newly cloned div so it can be populated
+      const currentItem = document.getElementById(currentClone)
+      console.log(currentItem)
+      console.log(currentItem.querySelector('.singleItemName'))
+      // currentItem.querySelector('.singleItemName').text('this')
+      currentItem.querySelector('.singleItemName').innerHTML ='this'
+      // currentItem.querySelector('.singleItemName').val('this')
+      console.log(currentItem.singleItemName)
+      console.log(currentItem.first)
+      console.log(currentItem.getElementById('currentClone'))
+      currentItem.singleItemName.innerHTML(responseArray[i].name)
+      console.log($('.container:nth-last-child'))
+      const thisItem = mainContainer.lastChildElement
+      console.log(thisItem)
+      // $('p:nth-last-child(1)').html(singleObject[0])
+      // $('p:nth-last-child(2)').html(singleObject[1])
+      // $('p:nth-last-child(3)').html(singleObject[2])
+      // $('p:nth-last-child(4)').html(singleObject[3])
+      // $('p:nth-last-child(5)').html(singleObject[4])
+      // $('p:nth-last-child(6)').html(singleObject[5])
+      // $('.singleItem:nth-last-child(1)').show()
    }
  }
+ createNewItems()
 }
 
 const loadInventoryFailure = function () {
